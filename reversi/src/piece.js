84223f1e525281fcf3468748a02c25157e4d62ab
galenddavis <73361653+galenddavis@users.noreply.github.com>
2020-12-17 11:38:22 -0500

    Start building reversi piece

--- reversi/src/piece.js
+++ reversi/src/piece.js
@@ -2,19 +2,29 @@
  * Initializes the Piece with its color.
  */
 function Piece (color) {
+    this.color = color;
 }
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+    if (this.color === 'white') {
+        return 'black';
+    } else {
+        return 'white';
+    }
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+    if (this.color === 'white') {
+        
+    }  
 };
+
 
 /**
  * Returns a string representation of the string
